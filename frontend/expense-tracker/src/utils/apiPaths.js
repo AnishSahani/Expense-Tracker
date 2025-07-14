53511ba841f8482c69a8a90415ede71738d9fee2
Anish Sahani <122989638+AnishSahani@users.noreply.github.com>
2025-07-15 00:21:39 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://expense-tracker-backend-u8vm.onrender.com";
 
 //utils/apiPath.js
 
